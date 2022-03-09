3e0f11f33bf2f4e9e9d00448cc66adcc74f15af7
CMZ33 <cmaxzana@gmail.com>
2022-03-09 22:48:47 +0200

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    if (principal<=0)
+    {
+        alert("We only take negetive numbers");
+        principal.focus();
+        return false;
+    }
+    var interest = (principal * years * rate) / 100;
     
-}
+    document.getElementById("all").hidden=false;
+    document.getElementById("result").innerText = principal;
+    document.getElementById("rates").innerText = rate;
+    document.getElementById("ammount").innerText = interest;
+  
+    function updateRate() {
+      var rateval = document.getElementById("rate").value;
+      document.getElementById("rate_val").innerText = rateval;
+    }
+  return true;
+  }     
         
